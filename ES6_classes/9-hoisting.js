32773fbfe7dcd6518610dcb61e5cefc53ac52c4d
q400400200-pixel <q.400400200@gmail.com>
2026-04-03 21:04:35 +0300

    Add files via upload

---
+++ ES6_classes/9-hoisting.js
@@ -0,0 +1,47 @@
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this.fullName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+export const class2019 = new HolbertonClass(2019, 'San Francisco');
+export const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+export const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+export const student2 = new StudentHolberton('John', 'Doe', class2020);
+export const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+export const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+export const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+export const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
---
+++ ES6_classes/README.md
@@ -0,0 +1,35 @@
+# ES6 Classes
+
+A project exploring ES6 Class syntax, inheritance, getters/setters, static methods, and metaprogramming with Symbols.
+
+## Learning Objectives
+
+- How to define a Class
+- How to add methods to a class
+- Why and how to add a static method to a class
+- How to extend a class from another
+- Metaprogramming and symbols
+
+## Setup
+
+```bash
+npm install
+npm run test
+npm run full-test
+```
+
+## Tasks Overview
+
+| File | Description |
+|------|-------------|
+| `0-classroom.js` | Basic class with constructor |
+| `1-make_classrooms.js` | Initialize array of ClassRoom instances |
+| `2-hbtn_course.js` | Class with getters & setters + type validation |
+| `3-currency.js` | Currency class with display method |
+| `4-pricing.js` | Pricing class with static method |
+| `5-building.js` | Abstract class pattern |
+| `6-sky_high.js` | Class inheritance with `extends` |
+| `7-airport.js` | Symbol.toStringTag metaprogramming |
+| `8-hbtn_class.js` | Symbol.toPrimitive metaprogramming |
+| `9-hoisting.js` | Class hoisting fix |
+| `10-car.js` | Clone pattern with `new this.constructor()` |
